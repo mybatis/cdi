664cdbb1e91eb668aab3f7cdd4fb41a7a8ca7a83
Eduardo Macarron <eduardo.macarron@gmail.com>
2013-12-21 11:06:06 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,10 @@
+var NAVTREE_DATA =
+[ [ "org.mybatis.cdi", "org/mybatis/cdi/package-summary.html", [ [ "Description", "org/mybatis/cdi/package-descr.html", null, "" ], [ "Classes", null, [ [ "CDIUtils", "org/mybatis/cdi/CDIUtils.html", null, "" ], [ "Extension", "org/mybatis/cdi/Extension.html", null, "" ], [ "Extension.BeanKey", "org/mybatis/cdi/Extension.BeanKey.html", null, "" ], [ "JtaTransactionInterceptor", "org/mybatis/cdi/JtaTransactionInterceptor.html", null, "" ], [ "LocalTransactionInterceptor", "org/mybatis/cdi/LocalTransactionInterceptor.html", null, "" ], [ "MyBatisBean", "org/mybatis/cdi/MyBatisBean.html", null, "" ], [ "SqlSessionManagerRegistry", "org/mybatis/cdi/SqlSessionManagerRegistry.html", null, "" ] ]
+, "" ], [ "Annotations", null, [ [ "Mapper", "org/mybatis/cdi/Mapper.html", null, "" ], [ "Transactional", "org/mybatis/cdi/Transactional.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "Isolation", "org/mybatis/cdi/Isolation.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "MybatisCdiConfigurationException", "org/mybatis/cdi/MybatisCdiConfigurationException.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,15 @@
+var DATA = [
+      { id:0, label:"org.mybatis.cdi", link:"org/mybatis/cdi/package-summary.html", type:"package" },
+      { id:1, label:"org.mybatis.cdi.CDIUtils", link:"org/mybatis/cdi/CDIUtils.html", type:"class" },
+      { id:2, label:"org.mybatis.cdi.Extension", link:"org/mybatis/cdi/Extension.html", type:"class" },
+      { id:3, label:"org.mybatis.cdi.Extension.BeanKey", link:"org/mybatis/cdi/Extension.BeanKey.html", type:"class" },
+      { id:4, label:"org.mybatis.cdi.Isolation", link:"org/mybatis/cdi/Isolation.html", type:"class" },
+      { id:5, label:"org.mybatis.cdi.JtaTransactionInterceptor", link:"org/mybatis/cdi/JtaTransactionInterceptor.html", type:"class" },
+      { id:6, label:"org.mybatis.cdi.LocalTransactionInterceptor", link:"org/mybatis/cdi/LocalTransactionInterceptor.html", type:"class" },
+      { id:7, label:"org.mybatis.cdi.Mapper", link:"org/mybatis/cdi/Mapper.html", type:"class" },
+      { id:8, label:"org.mybatis.cdi.MyBatisBean", link:"org/mybatis/cdi/MyBatisBean.html", type:"class" },
+      { id:9, label:"org.mybatis.cdi.MybatisCdiConfigurationException", link:"org/mybatis/cdi/MybatisCdiConfigurationException.html", type:"class" },
+      { id:10, label:"org.mybatis.cdi.SqlSessionManagerRegistry", link:"org/mybatis/cdi/SqlSessionManagerRegistry.html", type:"class" },
+      { id:11, label:"org.mybatis.cdi.Transactional", link:"org/mybatis/cdi/Transactional.html", type:"class" }
+
+    ];
