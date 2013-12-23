620005d2d1ef7077617477db813c609088a4d401
Eduardo Macarron <eduardo.macarron@gmail.com>
2013-12-23 09:34:03 +0100

    It is the producer what need to be @ApplicationScoped

--- src/test/java/org/mybatis/cdi/ManagerProducers.java
+++ src/test/java/org/mybatis/cdi/ManagerProducers.java
@@ -29,7 +29,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-@ApplicationScoped
 public class ManagerProducers {
 
   private SqlSessionFactory createSessionManager(int n) throws IOException {
